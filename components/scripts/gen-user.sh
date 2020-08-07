8d46d9e5890ab2521da7ebc2b12fbbf9ff4bf238
sam <sam@freighttrust.com>
2020-08-06 23:24:24 -0700

    feature: vaultctl python controller
    python based vault controller

---
+++ components/scripts/gen-user.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+unset username
+unset password
+echo -n "username:"
+read username
+prompt="password:"
+while IFS= read -p "$prompt" -r -s -n 1 char
+do
+    if [[ $char == $'\0' ]]
+    then
+         break
+    fi
+    prompt='*'
+    password+="$char"
+    
+done
+
+exit 1
